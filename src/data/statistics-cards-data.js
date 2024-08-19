b77ae130b29b4fb42997ea6a2bc4a5447a56af99
Jocker200 <seybasangare@yahoo.fr>
2024-08-20 00:30:17 +0100

    uploading dahsboard

---
+++ src/data/statistics-cards-data.js
@@ -0,0 +1,150 @@
+import {
+  BanknotesIcon,
+  UserPlusIcon,
+  UsersIcon,
+  ChartBarIcon,
+} from "@heroicons/react/24/solid";
+
+export const statisticsCardsData = [
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Janvier",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Fevrier",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Mars",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Avril",
+    },
+  },
+
+  /*-------------------------------------------------------------------*/
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Mai",
+    },
+  },
+
+    {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Juin",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Juillet",
+    },
+  },
+
+    {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Aout",
+    },
+  },
+  
+  /*----------------------------------------------------------------*/
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Septembre",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Octobre",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Novembre",
+    },
+  },
+  {
+    color: "gray",
+    icon: BanknotesIcon,
+    title: "",
+    value: "$53k",
+    footer: {
+      color: "text-green-500",
+      value: "+55%",
+      label: "Decembre",
+    },
+  },
+
+];
+
+export default statisticsCardsData;
---
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -0,0 +1,73 @@
+import { useLocation, Link } from "react-router-dom";
+import {
+  Navbar,
+  Typography,
+  Button,
+  IconButton,
+  Breadcrumbs,
+  Option,Select,
+  Input,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+} from "@material-tailwind/react";
+import {
+  UserCircleIcon,
+  Cog6ToothIcon,
+  BellIcon,
+  ClockIcon,
+  CreditCardIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/solid";
+import {
+  useMaterialTailwindController,
+  setOpenConfigurator,
+  setOpenSidenav,
+} from "@/context";
+
+export function DashboardNavbar() {
+  const [controller, dispatch] = useMaterialTailwindController();
+  const { fixedNavbar, openSidenav } = controller;
+  const { pathname } = useLocation();
+  const [layout, page] = pathname.split("/").filter((el) => el !== "");
+
+  return (
+    <Navbar
+      color={fixedNavbar ? "white" : "transparent"}
+      className={`rounded-xl transition-all ${
+        fixedNavbar
+          ? "sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
+          : "px-0 py-1"
+      }`}
+      fullWidth
+      blurred={fixedNavbar}
+    >
+      {/*debut barre de selection */}
+
+      <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
+      <div className="w-full">
+      <Select label={<h2>Selectionner une campagne</h2>}>
+        <Option>Material Tailwind HTML</Option>
+        <Option>Material Tailwind React</Option>
+        <Option>Material Tailwind Vue</Option>
+        <Option>Material Tailwind Angular</Option>
+        <Option>Material Tailwind Svelte</Option>
+      </Select>
+    </div>
+    
+     {/*Fin barre de selection */}
+
+    <div className="mr-auto md:mr-4 md:w-56">
+          <h3 className=" text-blue-gray-500">Dernieres modifications  : Nom Prenom <br/> 1/1/24 a 10:39</h3> 
+          </div>
+
+      </div>
+    </Navbar>
+  );
+}
+
+DashboardNavbar.displayName = "/src/widgets/layout/dashboard-navbar.jsx";
+
+export default DashboardNavbar;
